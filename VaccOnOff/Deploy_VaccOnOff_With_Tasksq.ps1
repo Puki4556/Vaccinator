5da6b00bb5f77934a3f6eff7a3ffac72c62591ae
DuckInCyber <37802070+DuckInCyber@users.noreply.github.com>
2018-08-14 15:26:34 +0300

    Update Deploy_VaccOnOff_With_Tasksq.ps1

--- VaccOnOff/Deploy_VaccOnOff_With_Tasksq.ps1
+++ VaccOnOff/Deploy_VaccOnOff_With_Tasksq.ps1
@@ -6,13 +6,17 @@ $comps = Get-Content "C:\comps.txt"
 # !!!!!! This value must be identical to the value in the Task_creater script !!!!!!!
 $TaskDescr = "onoff"
 
-# Path to the task_creater_VaccOnOff.ps1 script
+# Path to the VaccOnOff script
 $ps1Path = "C:\path\to\script\task_creater_VaccOnOff.ps1"
+
+# Save the failed computers
+$failed_comps = @()
 ###########################################################################
 foreach($comp in $comps)
 {
+    $flag = 0
     #$comp = $comp.name
-    if(Test-Connection $comp -Count 1)
+    if(Test-Connection $comp -Count 1 -ErrorAction SilentlyContinue)
     {
         Write-Host $comp
 
@@ -21,14 +25,23 @@ foreach($comp in $comps)
 
         # execute the script on the other computer
         # the script creates a task
-        Invoke-Command -ComputerName $comp -FilePath $ps1Path >> $null
-        sleep -Seconds 1
+        try{Invoke-Command -ComputerName $comp -FilePath $ps1Path >> $null}
+        catch{$failed_comps += $comp
+        $flag = 1
+        }
+
+        # If succeed
+        if($flag -eq 0)
+        {
+        sleep -Milliseconds 200
 
         # run the task
         schtasks /RUN /S $comp /I /TN $TaskDescr ## Put here the task name frome the task_creater ps1
-        sleep -Seconds 1
+        sleep -Milliseconds 200
 
         # delete the task
         schtasks /DELETE /S $comp /F /TN $TaskDescr ## Put here the task name frome the task_creater ps1
+        }
+
     }
 } 
